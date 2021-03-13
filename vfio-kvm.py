459f3fb6834457b287ae4370691025d5e438a5ee
Melissa Nuño <dangle@users.noreply.github.com>
2021-03-13 07:26:33 -0700

    Toggle through a list of VMs.

--- vfio-kvm.py
+++ vfio-kvm.py
@@ -7,21 +7,18 @@ import functools
 import logging
 import os
 import signal
-import sys
-import time
 
 import dbus_next as dbus
 import evdev
 import yaml
 
 
-VmConfig = collections.namedtuple("VmConfig", ("devices", "cpu", "memory", "hugepages"))
+VmConfig = collections.namedtuple(
+    "VmConfig", ("devices", "cpu", "hugepages1G", "hugepages2M")
+)
 
 
 class VfioKvmService(dbus.service.ServiceInterface):
-    _HOST = "host"
-    _GUEST = "guest"
-
     _DEFAULT_CONFIG_PATH = "/etc/vfio-kvm.yaml"
     _DEFAULT_QEMU_HOTKEY = ("KEY_LEFTCTRL", "KEY_RIGHTCTRL")
     _DEFAULT_HOTKEY = _DEFAULT_QEMU_HOTKEY
@@ -41,11 +38,12 @@ class VfioKvmService(dbus.service.ServiceInterface):
         if os.path.isfile(config):
             with open(config) as fp:
                 data = yaml.safe_load(fp)
-        self._target = self._HOST
+        self._targets = [None]
+        self._index = 0
+        self._target = self._targets[self._index]
         self._configure_hotkey(data.get("hotkey"))
         self._configure_qemu_hotkey(data.get("qemu_hotkey"))
         await self._configure_dbus(bus or self._BUS_NAME, path or self._OBJ_PATH)
-        self._target = self._HOST
         self._devices = {}
         logging.info("Listening for libvirtd events")
 
@@ -59,13 +57,6 @@ class VfioKvmService(dbus.service.ServiceInterface):
             evdev.ecodes.ecodes[key] for key in keys or self._DEFAULT_QEMU_HOTKEY
         )
 
-    def _configure_devices(self, devices):
-        if not devices:
-            logging.error("No devices configured")
-            sys.exit(6)
-        self.target = self._HOST
-        self._devices = [ReplicatedDevice(device, self) for device in devices or ()]
-
     async def _configure_dbus(self, bus, path):
         _bus = await dbus.aio.MessageBus(
             bus_type=dbus.constants.BusType.SYSTEM
@@ -75,17 +66,16 @@ class VfioKvmService(dbus.service.ServiceInterface):
         await _bus.request_name(bus)
         logging.debug("Bus name %s granted", _bus.unique_name)
 
-    def _grab_all(self):
-        for device in self._devices:
-            device.grab()
-
     def _parse_xml(self, xml_config: str) -> VmConfig:
         root = xml.fromstring(xml_config)
         cpu_pinnings = [
             int(e.get("cpuset")) for e in root.findall(".//cputune/vcpupin")
         ]
-        memory = int(root.findtext(".//memory"))
         hugepages = root.find(".//memoryBacking/hugepages") is not None
+        memory = int(root.findtext(".//memory"))
+        mem_in_mb = memory // 1024
+        gb_pages = mem_in_mb // 1024 if hugepages else 0
+        mb_pages = mem_in_mb % gb_pages if hugepages else 0
         devices = [
             param[6:]
             for e in root.findall(
@@ -96,7 +86,7 @@ class VfioKvmService(dbus.service.ServiceInterface):
             for param in e.get("value").split(",")
             if param.startswith("evdev=")
         ]
-        return VmConfig(devices, cpu_pinnings, memory, hugepages)
+        return VmConfig(devices, cpu_pinnings, gb_pages, mb_pages)
 
     @functools.cached_property
     def hotkey(self) -> frozenset:
@@ -106,31 +96,26 @@ class VfioKvmService(dbus.service.ServiceInterface):
     def qemu_hotkey(self) -> frozenset:
         return frozenset(self._qemu_hotkey)
 
+    def stop(self):
+        for device in self._devices.values():
+            device.stop()
+
     @dbus.service.dbus_property(name="Target")
     def target(self) -> "s":
         return self._target
 
     @target.setter
     def target(self, val: "s"):
-        if val == self._GUEST and not all(d.is_grabbed for d in self._devices):
-            self._grab_all()
+        logging.info("%s selected", val or "host device")
         self._target = val
-        logging.info("%s selected", self._target.upper())
-        self.emit_properties_changed({"Target": self._target})
-
-    @dbus.service.method("Start")
-    def start(self):
-        for device in self._devices:
-            device.start()
-
-    @dbus.service.method("Stop")
-    def stop(self):
-        for device in self._devices:
-            device.stop()
+        for device in self._devices.values():
+            device.grab()
+        self.emit_properties_changed({"Target": self._target or "host device"})
 
     @dbus.service.method("Toggle")
     def toggle(self) -> "s":
-        self.target = self._HOST if self.target == self._GUEST else self._GUEST
+        self._index = (self._index + 1) % len(self._targets)
+        self.target = self._targets[self._index]
         return self.target
 
     @dbus.service.method("Prepare")
@@ -138,47 +123,80 @@ class VfioKvmService(dbus.service.ServiceInterface):
         logging.info("VM %s starting up", vm_name)
         logging.debug("libvirtd: %s %s %s\n%s", vm_name, sub_op, extra_op, xml_config)
         config = self._parse_xml(xml_config)
+        self._targets.append(vm_name)
         self._pin_cpus(config.cpu)
-        self._allocate_hugepages(config.memory, config.hugepages)
+        self._allocate_hugepages(config.hugepages1G, config.hugepages2M)
         self._create_devices(vm_name, config.devices)
         return True
 
     def _pin_cpus(self, cpu):
-        logging.info("Pinning CPUs: %s", cpu)
+        if not cpu:
+            return
+        logging.info("Pinning CPUs: %s", ", ".join(str(c) for c in sorted(cpu)))
 
-    def _allocate_hugepages(self, memory, hugepages):
-        if hugepages:
-            logging.info("Allocating hugepages: %d bytes", memory)
+    def _allocate_hugepages(self, gb_pages, mb_pages):
+        if not gb_pages and not mb_pages:
+            return
+        logging.info(
+            "Allocating %d 1G hugepages and %d 2M hugepages", gb_pages, mb_pages
+        )
 
     def _create_devices(self, vm_name, devices):
-        for device in devices:
-            logging.info(
-                "Creating device %s/%s-%s",
-                os.path.join(os.sep, "dev", "input", "by-id"),
-                vm_name,
-                os.path.basename(device),
+        for guest_source in devices:
+            source = os.path.join(
+                os.sep,
+                "dev",
+                "input",
+                "by-id",
+                os.path.basename(guest_source)[len(vm_name) + 1 :],
             )
+            if source not in self._devices:
+                self._devices[source] = ReplicatedDevice(source, self)
+            device = self._devices.get(source)
+            device.add(vm_name)
 
     @dbus.service.method("Release")
     def release(self, vm_name: "s", sub_op: "s", extra_op: "s", xml_config: "s") -> "b":
+        if vm_name not in self._targets:
+            logging.warning("Attempted to release devices for unmanaged VM %s", vm_name)
+            return False
         logging.info("VM %s shutting down", vm_name)
         logging.debug("libvirtd: %s %s %s\n%s", vm_name, sub_op, extra_op, xml_config)
         config = self._parse_xml(xml_config)
         self._destroy_devices(vm_name, config.devices)
-        self._deallocate_hugepages(config.memory, config.hugepages)
+        self._deallocate_hugepages(config.hugepages1G, config.hugepages2M)
         self._unpin_cpus(config.cpu)
+        self._targets.remove(vm_name)
         return True
 
     def _unpin_cpus(self, cpu):
-        logging.info("Unpinning CPUs: %s", cpu)
+        if not cpu:
+            return
+        logging.info("Unpinning CPUs: %s", ", ".join(str(c) for c in sorted(cpu)))
 
-    def _deallocate_hugepages(self, memory, hugepages):
-        if hugepages:
-            logging.info("Deallocating hugepages: %d bytes", memory)
+    def _deallocate_hugepages(self, gb_pages, mb_pages):
+        if not gb_pages and not mb_pages:
+            return
+        logging.info(
+            "Deallocating %d 1G hugepages and %d 2M hugepages", gb_pages, mb_pages
+        )
 
     def _destroy_devices(self, vm_name, devices):
-        for device in devices:
-            logging.info("Destroying device %s-%s", vm_name, device)
+        for guest_source in devices:
+            source = os.path.join(
+                os.sep,
+                "dev",
+                "input",
+                "by-id",
+                os.path.basename(guest_source)[len(vm_name) + 1 :],
+            )
+            if source not in self._devices:
+                logging.warning(
+                    "Attempted to destroy non-existent device %s", guest_source
+                )
+            else:
+                device = self._devices[source]
+                device.remove(vm_name)
 
 
 class ReplicatedDevice:
@@ -186,12 +204,13 @@ class ReplicatedDevice:
         self._name = os.path.basename(source)
         self._source = evdev.InputDevice(source)
         self._manager = manager
-        self._task = None
-        self._host = None
-        self._guest = None
-        self._devices = {}
+        self._targets = {}
+        self._grab_task = None
+        self._replicate_task = None
+        self._create_device("host", key=None)
+        self.start()
 
-    def _get_device_path(self, target) -> str:
+    def _get_device_path(self, target):
         return os.path.join(
             os.sep,
             "dev",
@@ -200,69 +219,40 @@ class ReplicatedDevice:
             f"{target}-{self._name}",
         )
 
-    @functools.cached_property
-    def _host_path(self) -> str:
-        return self._get_device_path("host")
+    def _create_device(self, target, *, key=False):
+        path = self._get_device_path(target)
+        logging.info(f"Creating {target} device %s", path)
+        device = evdev.UInput.from_device(self._source)
+        self._targets[key if key is not False else target] = device
+        if os.path.islink(path):
+            logging.debug(f"Removing existing symlink %s", path)
+            os.unlink(path)
+        os.symlink(device.device, path)
 
-    @functools.cached_property
-    def _guest_path(self) -> str:
-        return self._get_device_path("guest")
+    def _destroy_device(self, target, *, key=False):
+        path = self._get_device_path(target)
+        if os.path.islink(path):
+            logging.debug(f"Removing symlink %s", path)
+            os.unlink(path)
+        self._targets[key if key is not False else target].close()
+        # del self._targets[key if key is not False else target]
+
+    async def _grab_source(self):
+        while 1:
+            try:
+                self._source.grab()
+                logging.debug("Grabbed source device %s", self._source.path)
+            except IOError:
+                pass
+            except asyncio.CancelledError:
+                return
+            await asyncio.sleep(5)
 
     @property
     def _target(self) -> evdev.device.InputDevice:
-        return getattr(self, f"_{self._manager.target}")
-
-    def _link(self) -> None:
-        logging.info(f"Creating host device %s", self._host_path)
-        os.symlink(self._host.device, self._host_path)
-        logging.info(f"Creating guest device %s", self._guest_path)
-        os.symlink(self._guest.device, self._guest_path)
-
-    def _unlink(self) -> None:
-        if os.path.islink(self._host_path):
-            logging.info(f"Removing host device %s", self._host_path)
-            os.unlink(self._host_path)
-        if os.path.islink(self._guest_path):
-            logging.info(f"Removing guest device %s", self._guest_path)
-            os.unlink(self._guest_path)
-
-    def _init_device(self):
-        self._host = evdev.UInput.from_device(self._source)
-        self._guest = evdev.UInput.from_device(self._source)
-        self._unlink()
-        self._link()
-        try:
-            self._source.grab()
-        except IOError as e:
-            logging.exception(f"Unable to grab device %s", self._source.path)
-            asyncio.get_event_loop().stop()
-            sys.exit(1)
-        logging.info(f"Listening to device %s", self._source.path)
-
-    def _cleanup_device(self):
-        self._source.ungrab()
-        self._unlink()
-        self._host.close()
-        self._guest.close()
-        logging.info(f"No longer listening to device %s", self._source.path)
-
-    @property
-    def is_grabbed(self):
-        try:
-            self._guest.device.grab()
-        except IOError:
-            return True
-        self._guest.device.ungrab()
-        return False
-
-    def grab(self):
-        for value in (1, 0):
-            for key in self._manager.qemu_hotkey:
-                self._guest.write(evdev.ecodes.EV_KEY, key, value)
-        self._guest.syn()
+        return self._targets.get(self._manager.target)
 
     async def _replicate(self) -> None:
-        self._init_device()
         try:
             is_toggle = False
             async for event in self._source.async_read_loop():
@@ -276,17 +266,55 @@ class ReplicatedDevice:
                         is_toggle = True
                     elif is_toggle and not self._source.active_keys():
                         self._target.syn()  # Flush queued write events
-                        time.sleep(0.1)  # Wait for events to flush
+                        await asyncio.sleep(0.1)  # Wait for events to flush
                         is_toggle = False
                         self._manager.toggle()
         except asyncio.CancelledError:
-            self._cleanup_device()
+            return
+
+    @property
+    def _is_grabbed(self):
+        try:
+            self._target.device.grab()
+        except IOError:
+            return True
+        self._target.device.ungrab()
+        return False
+
+    def grab(self):
+        if not self._manager.target or self._is_grabbed:
+            return
+        logging.debug("Grabbing device %s", self._get_device_path(self._manager.target))
+        for value in (1, 0):
+            for key in self._manager.qemu_hotkey:
+                self._target.write(evdev.ecodes.EV_KEY, key, value)
+        self._target.syn()
 
     def start(self) -> None:
-        self._task = asyncio.create_task(self._replicate())
+        if not self._grab_task:
+            self._grab_task = asyncio.create_task(self._grab_source())
+        if not self._replicate_task:
+            self._replicate_task = asyncio.create_task(self._replicate())
 
     def stop(self) -> None:
-        self._task.cancel()
+        self._replicate_task.cancel()
+        self._grab_task.cancel()
+        try:
+            self._source.ungrab()
+        except IOError:
+            pass
+        for target in frozenset(self._targets):
+            self._destroy_device(target if target else "host", key=target)
+        logging.info(f"Ungrabbed device %s", self._source.path)
+
+    def add(self, vm_name: str) -> None:
+        self._create_device(vm_name)
+        self.start()
+
+    def remove(self, vm_name: str) -> None:
+        self._destroy_device(vm_name)
+        if len(self._targets) == 1:
+            self.stop()
 
 
 async def main() -> None:
@@ -302,8 +330,6 @@ async def main() -> None:
 
     for s in (signal.SIGINT, signal.SIGQUIT, signal.SIGTERM):
         asyncio.get_event_loop().add_signal_handler(s, signal_handler)
-
-    manager.start()
 
 
 if __name__ == "__main__":
