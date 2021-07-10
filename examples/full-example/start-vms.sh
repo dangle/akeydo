a523ffbd1a4caee139ca4a525b70da11561eb5f9
Melissa Nuño <dangle@users.noreply.github.com>
2021-07-10 08:40:21 -0600

    Add a full example of a start script based on my own.

---
+++ examples/full-example/start-vms.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+UPPER_MONITOR=dev:/dev/i2c-4
+LOWER_MONITOR=dev:/dev/i2c-6
+RIGHT_MONITOR=dev:/dev/i2c-7
+
+INPUT_REGISTER=0x60
+
+HDMI1=0x11
+HDMI2=0x12
+DP=0x0f
+USBC=0x1b
+
+VM=( $(virsh list | awk 'NR>2 { print $2 }') )
+
+for vm in ${VM[@]}; do
+  virsh start "${vm}"
+done
+
+HOST="$(virsh domifaddr "${VM[0]}" | awk '/192.168./{print substr($4, 1, length($4)-3)}')"
+
+function change_input () {
+  local monitor=${1}
+  local new_input=${2}
+  ssh ${HOST} bash <<EOF
+raw_current_input=\$(ddccontrol -r ${INPUT_REGISTER} ${monitor} | awk -F '/' '/+/{ print \$2 }')
+current_input=\$((\$raw_current_input - 0x0F00))
+
+if (( \${current_input} != ${new_input} )); then
+  ddccontrol -r ${INPUT_REGISTER} -w ${new_input} ${monitor}
+fi
+EOF
+}
+
+dbus-monitor --system "type='signal',sender='dev.akeydo'" 2>/dev/null |
+  while read line; do
+    case "${line}" in
+    *"string \"${VM[0]}\""*)
+      change_input ${LOWER_MONITOR} ${DP}
+      change_input ${UPPER_MONITOR} ${DP}
+      ;;
+    *"string \"${VM[1]}\""*)
+      change_input ${LOWER_MONITOR} ${HDMI1}
+      ;;
+    *"string \"${VM[2]}\""*)
+      change_input ${UPPER_MONITOR} ${HDMI2}
+      ;;
+    esac
+  done
