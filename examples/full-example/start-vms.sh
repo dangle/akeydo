39768a5f9a4c8c052844dd64e2546d7130fd005e
Melissa Nuño <dangle@users.noreply.github.com>
2021-07-11 21:07:03 -0600

    Update the full example to match my real setup.

--- examples/full-example/start-vms.sh
+++ examples/full-example/start-vms.sh
@@ -8,8 +8,8 @@ INPUT_REGISTER=0x60
 
 HDMI1=0x11
 HDMI2=0x12
-DP=0x0f
-USBC=0x1b
+DP=0x0F
+USBC=0x1B
 
 VM=($(virsh list --inactive --name))
 
@@ -17,7 +17,7 @@ for vm in ${VM[@]}; do
   virsh start "${vm}"
 done
 
-HOST="$(virsh domifaddr "${VM[0]}" | awk '/192.168./{print substr($4, 1, length($4)-3)}')"
+HOST="$(virsh domifaddr "${VM[1]}" | awk '/192.168./{print substr($4, 1, length($4)-3)}')"
 
 function change_input() {
   local monitor=${1}
@@ -36,11 +36,11 @@ dbus-monitor --system "type='signal',sender='dev.akeydo'" 2>/dev/null |
   while read line; do
     case "${line}" in
     *"string \"${VM[0]}\""*)
-      change_input ${LOWER_MONITOR} ${DP}
-      change_input ${UPPER_MONITOR} ${DP}
+      change_input ${LOWER_MONITOR} ${HDMI1}
       ;;
     *"string \"${VM[1]}\""*)
-      change_input ${LOWER_MONITOR} ${HDMI1}
+      change_input ${LOWER_MONITOR} ${DP}
+      change_input ${UPPER_MONITOR} ${DP}
       ;;
     *"string \"${VM[2]}\""*)
       change_input ${UPPER_MONITOR} ${HDMI2}
