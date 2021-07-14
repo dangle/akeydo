902a64a40262478f9066614d513d3c3cafdc1f50
Melissa Nuño <dangle@users.noreply.github.com>
2021-07-14 15:30:58 -0600

    Add initial CPU slicing for cgroupsv2

--- akeydo/plugins/cpu/drivers/cgroup2.py
+++ akeydo/plugins/cpu/drivers/cgroup2.py
@@ -1,9 +1,29 @@
+from ....system import system
+
+
 class Driver:
-    def __init__(self, cores: int) -> None:
-        self._cores = cores
+    _HOST_CGROUPS = (
+        "init.scope",
+        "user.slice",
+        "system.slice",
+    )
+
+    def __init__(self, cores: int, path: str) -> None:
+        self._all_cpus = set(range(self.cores))
+        self._vm_cpus = set()
+        self._path = path
+        system.write(f"{self._path}/cgroup.subtree_control", "+cpuset")
 
     def shield_cpu(self, *cpu):
-        pass
+        self._vm_cpus.update(cpu)
+        self._set_host_cpus()
 
     def unshield_cpu(self, *cpu):
-        pass
+        self._vm_cpus.difference_update(cpu)
+        self._set_host_cpus()
+
+    def _set_host_cpus(self) -> str:
+        host_cpus = self._all_cpus - self._vm_cpus
+        config = ",".join(host_cpus)
+        for cgroup in self._HOST_CGROUPS:
+            system.write(f"{self._path}/{cgroup}/cpuset.cpus", config)
--- akeydo/system.py
+++ akeydo/system.py
@@ -2,12 +2,18 @@ class _System:
     def __init__(self) -> None:
         self._values = {}
 
-    def set(self, path: str, value, parse=None):
+    def read(self, path: str, parse=None) -> str:
         with open(path) as file:
-            contents = path.read()
-        self._values[path] = parse(contents) if parse else contents
+            contents = file.read()
+        return parse(contents) if parse else contents
+
+    def write(self, path: str, value) -> None:
         with open(path, "w") as file:
             file.write(f"{value}")
+
+    def set(self, path: str, value, parse=None):
+        self._values[path] = self.get(path, parse)
+        self.write(path, value)
         return self._values[path]
 
     def reset(self, path: str):
