71e5cce61c4935d2ae20148e12ea02daa573e0f6
Melissa Nuño <dangle@users.noreply.github.com>
2021-07-13 19:20:02 -0600

    Start the framework for managing the CPU.

---
+++ akeydo/plugins/cpu/drivers/systemd.py
@@ -0,0 +1,9 @@
+class Driver:
+    def __init__(self, cores: int) -> None:
+        self._cores = cores
+
+    def shield_cpu(self, *cpu):
+        pass
+
+    def unshield_cpu(self, *cpu):
+        pass
--- akeydo/plugins/cpu/manager.py
+++ akeydo/plugins/cpu/manager.py
@@ -6,6 +6,7 @@ Classes:
 
 from __future__ import annotations
 
+import functools
 import logging
 
 
@@ -17,36 +18,54 @@ class Manager:
             settings: Global settings for hotkeys and plug-in options.
         """
         self._settings: Settings = settings
+        self._driver = self._get_driver()
+        self._shielded_vms = 0
 
-    async def vm_prepare(self, vm_name: str, config: VirtualMachineConfig) -> None:
-        """Restrict kernel processes to pinned CPUs.
-
-        If the "manage_cpu" option is enabled, it will set cpusets to restrict
-        the kernel from adding processes to the pinned CPUs.
-
-        Arg:
-            cpu: A tuple of integers of CPUs to restrict. These should match up
-                to pinned CPUs from the virtual machine XML configuration.
-        """
-        if not self._settings.cpu.manage or not config.pinned_cpus:
+    async def vm_prepare(self, _: str, config: VirtualMachineConfig) -> None:
+        """Restrict kernel processes to pinned CPUs."""
+        if not config.pinned_cpus:
             return
         logging.info(
             "Pinning CPUs: %s", ", ".join(str(c) for c in sorted(config.pinned_cpus))
         )
+        driver = self._get_driver()
+        driver.shield_cpu(*config.pinned_cpus)
+        if not self._shielded_vms:
+            pass
+        self._shielded_vms += 1
 
-    async def vm_release(self, vm_name: str, config: VirtualMachineConfig) -> None:
-        """Remove process restrictions to CPUs used by the the virtual machine.
-
-        If the "manage_cpu" option is enabled, it will set cpusets to allow
-        the kernel to add processes to the CPUs that were pinned by the virtual
-        machine.
-
-        Arg:
-            cpu: A tuple of integers of CPUs to allow. These should match up
-                to pinned CPUs from the virtual machine XML configuration.
-        """
-        if not self._settings.cpu.manage or not config.pinned_cpus:
+    async def vm_release(self, _: str, config: VirtualMachineConfig) -> None:
+        """Remove process restrictions to CPUs used by the the virtual machine."""
+        if not config.pinned_cpus:
             return
         logging.info(
             "Unpinning CPUs: %s", ", ".join(str(c) for c in sorted(config.pinned_cpus))
         )
+        driver = self._get_driver()
+        driver.unshield_cpu(*config.pinned_cpus)
+        self._shielded_vms -= 1
+        if not self._shielded_vms:
+            pass
+
+    @functools.cached_property
+    def _is_cgroups_v2(self):
+        with open("/proc/mount") as file:
+            for line in file.readlines():
+                if line.startswith("cgroups2 /sys/fs/cgroup"):
+                    return True
+        return False
+
+    @functools.cached_property
+    def _cpu_cores(self):
+        with open("/proc/cpuinfo") as file:
+            for line in file.readlines():
+                if line.startswith("cpu cores"):
+                    return int(line.split(":")[1].strip())
+
+    @functools.cached_property
+    def _driver(self):
+        if self._is_cgroups_v2:
+            from .drivers.systemd import Driver
+        else:
+            from .drivers.cset import Driver
+        return Driver(self._cpu_cores)
